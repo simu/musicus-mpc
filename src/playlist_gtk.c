1aba18f5b502d083b767027f7074ec615808c900
simu <simu@776f24d4-6f41-0410-b491-ebdf431229a9>
2008-03-08 21:07:53 +0000

    Fixed indenting in playlist_gtk.c
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/musicus-mpc/trunk@20 776f24d4-6f41-0410-b491-ebdf431229a9

--- src/playlist_gtk.c
+++ src/playlist_gtk.c
@@ -41,140 +41,140 @@ static GtkWidget *PlTreeView;
 /* initalize playlist widget */
 GtkWidget *init_pl_widget(void) {
 
-	GtkWidget *pl_widget, *pl_tree_view, *scrolled_window;
-	GtkTreeStore *pl_tree_store;
-	GtkObject *h_adjustment, *v_adjustment;
+    GtkWidget *pl_widget, *pl_tree_view, *scrolled_window;
+    GtkTreeStore *pl_tree_store;
+    GtkObject *h_adjustment, *v_adjustment;
     GtkTreeModel *tree_model;
     GtkTreeSelection *tree_selection;
 
-	MpdPlContainer *plc;
+    MpdPlContainer *plc;
 
-	if(debug) {
-		fprintf(log_file, "[%s:%3i] %s(): before mpd_get_pl()\n", __FILE__, __LINE__, __FUNCTION__);
-		fflush(log_file);
-	}
+    if(debug) {
+	fprintf(log_file, "[%s:%3i] %s(): before mpd_get_pl()\n", __FILE__, __LINE__, __FUNCTION__);
+	fflush(log_file);
+    }
 
-	plc = mpd_get_pl();
+    plc = mpd_get_pl();
 
-	if(debug) {
-		fprintf(log_file, "[%s:%3i] %s(): before pl_new()\n", __FILE__, __LINE__, __FUNCTION__);
-		fflush(log_file);
-	}
-	/* create the tree view with the mpd playlist and set appropriate global references */
-	if(plc == NULL) pl_tree_store = pl_empty_tree_store();
-	else {
-	    pl_tree_store = pl_create_tree_store(plc);
-	    mpd_pl_container_free(plc);
-	}
-	PlTreeStore = pl_tree_store;
-	pl_tree_view = pl_create_tree_view(GTK_TREE_MODEL(pl_tree_store));
-	gtk_tree_view_set_reorderable(GTK_TREE_VIEW(pl_tree_view), TRUE);
-	PlTreeView = pl_tree_view;
-	tree_model = gtk_tree_view_get_model(GTK_TREE_VIEW(pl_tree_view));
-	tree_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(pl_tree_view));
-	/* set up for double-click -> plays selected song */
-	gtk_tree_selection_set_mode(tree_selection, GTK_SELECTION_MULTIPLE);
-	g_signal_connect(G_OBJECT(pl_tree_view), "row-activated",
-					 G_CALLBACK(pl_row_activated), GTK_TREE_SELECTION(tree_selection));
-	g_signal_connect(G_OBJECT(pl_tree_view), "key-press-event",
-					 G_CALLBACK(pl_key_pressed), (gpointer)tree_selection);
+    if(debug) {
+	fprintf(log_file, "[%s:%3i] %s(): before pl_new()\n", __FILE__, __LINE__, __FUNCTION__);
+	fflush(log_file);
+    }
+    /* create the tree view with the mpd playlist and set appropriate global references */
+    if(plc == NULL) pl_tree_store = pl_empty_tree_store();
+    else {
+	pl_tree_store = pl_create_tree_store(plc);
+	mpd_pl_container_free(plc);
+    }
+    PlTreeStore = pl_tree_store;
+    pl_tree_view = pl_create_tree_view(GTK_TREE_MODEL(pl_tree_store));
+    gtk_tree_view_set_reorderable(GTK_TREE_VIEW(pl_tree_view), TRUE);
+    PlTreeView = pl_tree_view;
+    tree_model = gtk_tree_view_get_model(GTK_TREE_VIEW(pl_tree_view));
+    tree_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(pl_tree_view));
+    /* set up for double-click -> plays selected song */
+    gtk_tree_selection_set_mode(tree_selection, GTK_SELECTION_MULTIPLE);
+    g_signal_connect(G_OBJECT(pl_tree_view), "row-activated",
+				     G_CALLBACK(pl_row_activated), GTK_TREE_SELECTION(tree_selection));
+    g_signal_connect(G_OBJECT(pl_tree_view), "key-press-event",
+				     G_CALLBACK(pl_key_pressed), (gpointer)tree_selection);
 
-	if (debug) {
-		fprintf(log_file, "[%s:%3i] %s(): before adding scrolled window\n", __FILE__, __LINE__, __FUNCTION__);
-		fflush(log_file);
-	}
-	/* put a scrolled window in between the treeview and the window... */
-	/* I just don't know how this works exactly, but the result is acceptable, so why bother */
-	h_adjustment = gtk_adjustment_new(0, 0, 4, 1, 4, 4);
-	v_adjustment = gtk_adjustment_new(0, 0, 20, 1, 20, 20);
+    if (debug) {
+	fprintf(log_file, "[%s:%3i] %s(): before adding scrolled window\n", __FILE__, __LINE__, __FUNCTION__);
+	fflush(log_file);
+    }
+    /* put a scrolled window in between the treeview and the window... */
+    /* I just don't know how this works exactly, but the result is acceptable, so why bother */
+    h_adjustment = gtk_adjustment_new(0, 0, 4, 1, 4, 4);
+    v_adjustment = gtk_adjustment_new(0, 0, 20, 1, 20, 20);
 
-	scrolled_window = gtk_scrolled_window_new(GTK_ADJUSTMENT(h_adjustment), GTK_ADJUSTMENT(v_adjustment));
-	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolled_window), GTK_SHADOW_NONE);
-	/* display scrollbars only when needed */
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    scrolled_window = gtk_scrolled_window_new(GTK_ADJUSTMENT(h_adjustment), GTK_ADJUSTMENT(v_adjustment));
+    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolled_window), GTK_SHADOW_NONE);
+    /* display scrollbars only when needed */
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 
-	gtk_container_add(GTK_CONTAINER(scrolled_window), pl_tree_view);
-	pl_widget = scrolled_window;
+    gtk_container_add(GTK_CONTAINER(scrolled_window), pl_tree_view);
+    pl_widget = scrolled_window;
 
-	return pl_widget;
+    return pl_widget;
 }
 
 /* update playlist tree view */
 void playlist_update_tree(void) {
 
-	int old_debug;
+    int old_debug;
 
-	MpdPlContainer *plc;
+    MpdPlContainer *plc;
 
-	/* no local debug */
-   	old_debug = debug; debug = 0;
-	if (debug) {
-		fprintf(log_file, "[%s:%3i] %s() called\n", __FILE__, __LINE__, __FUNCTION__);
-		fflush(log_file);
-	}
-	/* get new playlist */
-	plc = mpd_get_pl();
+    /* no local debug */
+    old_debug = debug; debug = 0;
+    if (debug) {
+	fprintf(log_file, "[%s:%3i] %s() called\n", __FILE__, __LINE__, __FUNCTION__);
+	fflush(log_file);
+    }
+    /* get new playlist */
+    plc = mpd_get_pl();
 
-	/* only do this if a playlist window exists... */
-	if((PlTreeStore != NULL) && (PlTreeView != NULL)) {
-		/* reset and refill PlTreeStore and PlTreeView */
-		gtk_tree_store_clear(PlTreeStore);
-		if(plc == NULL) PlTreeStore = pl_empty_tree_store();
-		else {
-		    PlTreeStore = pl_create_tree_store(plc);
-		    mpd_pl_container_free(plc);
-		}
-
-		gtk_tree_view_set_model(GTK_TREE_VIEW(PlTreeView), GTK_TREE_MODEL(PlTreeStore));
+    /* only do this if a playlist window exists... */
+    if((PlTreeStore != NULL) && (PlTreeView != NULL)) {
+	/* reset and refill PlTreeStore and PlTreeView */
+	gtk_tree_store_clear(PlTreeStore);
+	if(plc == NULL) PlTreeStore = pl_empty_tree_store();
+	else {
+	    PlTreeStore = pl_create_tree_store(plc);
+	    mpd_pl_container_free(plc);
 	}
 
-	/* revert debug state */
-	debug = old_debug;
-	return;
+	gtk_tree_view_set_model(GTK_TREE_VIEW(PlTreeView), GTK_TREE_MODEL(PlTreeStore));
+    }
+
+    /* revert debug state */
+    debug = old_debug;
+    return;
 }
 
 /* delete songs from playlist */
 void pl_del_songs(GtkWidget *widget, gpointer data) {
-	GtkTreeIter iter;
-	int i, selection_length;
-	gint *pl_id;
-	GList *selection;
-	GtkTreeModel *tree_model;
+    GtkTreeIter iter;
+    int i, selection_length;
+    gint *pl_id;
+    GList *selection;
+    GtkTreeModel *tree_model;
 
-	pl_id = (gint *)-32;
+    pl_id = (gint *)-32;
 
-	selection = gtk_tree_selection_get_selected_rows(GTK_TREE_SELECTION(data), &tree_model);
-	selection_length = g_list_length(selection);
-	if(debug) {
-		fprintf(log_file, "[%s:%3i] %s(): selection length = %i\n", __FILE__, __LINE__, __FUNCTION__, selection_length);
-		fflush(log_file);
-	}
+    selection = gtk_tree_selection_get_selected_rows(GTK_TREE_SELECTION(data), &tree_model);
+    selection_length = g_list_length(selection);
+    if(debug) {
+	fprintf(log_file, "[%s:%3i] %s(): selection length = %i\n", __FILE__, __LINE__, __FUNCTION__, selection_length);
+	fflush(log_file);
+    }
 
     if(mpd_info.msi.connected) {
-        if(!mpd_check_connected(mpd_info.obj))
-            if(mpd_connect(mpd_info.obj) != MPD_OK) {
+	if(!mpd_check_connected(mpd_info.obj))
+	    if(mpd_connect(mpd_info.obj) != MPD_OK) {
 		msi_clear(&mpd_info);
 		mpd_info.msi.connected = FALSE;
 		return;
 	    }
-	    msi_fill(&mpd_info);
-	    mpd_info.msi.connected = TRUE;
+	msi_fill(&mpd_info);
+	mpd_info.msi.connected = TRUE;
     }
-	for(i=0; i<selection_length; i++) {
-		gtk_tree_model_get_iter(tree_model, &iter, selection->data);
-		gtk_tree_model_get(tree_model, &iter, COLUMN_PL_ID, &pl_id, -1);
-		if(debug) {
-			fprintf(log_file, "[%s:%3i] %s(): song id = %i\n", __FILE__, __LINE__, __FUNCTION__, (int)pl_id);
-			fflush(log_file);
-		}
-		mpd_playlist_queue_delete_id(mpd_info.obj, (int)pl_id);
-		selection = g_list_next(selection);
+    for(i=0; i<selection_length; i++) {
+	gtk_tree_model_get_iter(tree_model, &iter, selection->data);
+	gtk_tree_model_get(tree_model, &iter, COLUMN_PL_ID, &pl_id, -1);
+	if(debug) {
+	    fprintf(log_file, "[%s:%3i] %s(): song id = %i\n", __FILE__, __LINE__, __FUNCTION__, (int)pl_id);
+	    fflush(log_file);
 	}
-	mpd_playlist_queue_commit(mpd_info.obj);
-	fprintf(log_file, "[%s:%3i] %s(): finished deleting songs\n", __FILE__, __LINE__, __FUNCTION__);
-	g_list_foreach (selection, (GFunc)gtk_tree_path_free, NULL);
-	g_list_free (selection);
-	return;
+	mpd_playlist_queue_delete_id(mpd_info.obj, (int)pl_id);
+	selection = g_list_next(selection);
+    }
+    mpd_playlist_queue_commit(mpd_info.obj);
+    fprintf(log_file, "[%s:%3i] %s(): finished deleting songs\n", __FILE__, __LINE__, __FUNCTION__);
+    g_list_foreach (selection, (GFunc)gtk_tree_path_free, NULL);
+    g_list_free (selection);
+    return;
 }
 
 /**
@@ -200,73 +200,73 @@ void mpd_add_song_list(GtkWidget *widget, gpointer data) {
     gchar *file_name;
 
     if(debug) {
-		fprintf(log_file, "[%s:%3i] %s() called\n", __FILE__, __LINE__, __FUNCTION__);
+	fprintf(log_file, "[%s:%3i] %s() called\n", __FILE__, __LINE__, __FUNCTION__);
+	fflush(log_file);
+    }
+
+    selection = g_list_nth_data(argument_list, 1);
+    selected_rows = gtk_tree_selection_get_selected_rows(selection, &model);
+
+    /* anything selected ? */
+    if(g_list_length(selected_rows) > 0) {
+
+	if(debug) {
+	    fprintf(log_file, "[%s:%3i] %s(): %i songs selected\n", __FILE__, __LINE__, __FUNCTION__, g_list_length(selected_rows));
+	    fflush(log_file);
+	}
+	selected_rows = g_list_first(selected_rows);
+	while(selected_rows != NULL) {
+	    gtk_tree_model_get_iter(model, &iter, selected_rows->data);
+
+	    gtk_tree_model_get(model, &iter, COLUMN_FILE, &file_name, -1);
+	    if(debug) {
+		fprintf(log_file, "[%s:%3i] %s(): file_name = %s\n", __FILE__, __LINE__, __FUNCTION__, file_name);
 		fflush(log_file);
+	    }
+	    song_list = g_list_append(song_list, file_name);
+	    /* g_free(file_name); */
+	    selected_rows = g_list_next(selected_rows);
 	}
-
-	selection = g_list_nth_data(argument_list, 1);
-	selected_rows = gtk_tree_selection_get_selected_rows(selection, &model);
-
-	/* anything selected ? */
-	if(g_list_length(selected_rows) > 0) {
-
-		if(debug) {
-			fprintf(log_file, "[%s:%3i] %s(): %i songs selected\n", __FILE__, __LINE__, __FUNCTION__, g_list_length(selected_rows));
-			fflush(log_file);
-		}
-		selected_rows = g_list_first(selected_rows);
-		while(selected_rows != NULL) {
-			gtk_tree_model_get_iter(model, &iter, selected_rows->data);
-
-			gtk_tree_model_get(model, &iter, COLUMN_FILE, &file_name, -1);
-			if(debug) {
-				fprintf(log_file, "[%s:%3i] %s(): file_name = %s\n", __FILE__, __LINE__, __FUNCTION__, file_name);
-				fflush(log_file);
-			}
-			song_list = g_list_append(song_list, file_name);
-			/* g_free(file_name); */
-			selected_rows = g_list_next(selected_rows);
-		}
-		list_length = g_list_length(song_list);
-		/* free selected rows list */
-		g_list_foreach (selected_rows, (GFunc)gtk_tree_path_free, NULL);
-		g_list_free(selected_rows);
+	list_length = g_list_length(song_list);
+	/* free selected rows list */
+	g_list_foreach (selected_rows, (GFunc)gtk_tree_path_free, NULL);
+	g_list_free(selected_rows);
+    }
+    /* nothing selected, add all found songs; maybe not the best way, but never mind */
+    else {
+	song_list = g_list_nth_data(argument_list, 0);
+	list_length = g_list_length(song_list);
+	if(debug) {
+	    fprintf(log_file, "[%s:%3i] %s(): list_length = %i\n", __FILE__, __LINE__, __FUNCTION__, list_length);
+	    fflush(log_file);
 	}
-	/* nothing selected, add all found songs; maybe not the best way, but never mind */
-	else {
-		song_list = g_list_nth_data(argument_list, 0);
-		list_length = g_list_length(song_list);
-		if(debug) {
-			fprintf(log_file, "[%s:%3i] %s(): list_length = %i\n", __FILE__, __LINE__, __FUNCTION__, list_length);
-			fflush(log_file);
-		}
+    }
+
+    /* add songs */
+
+    for(i = 0; i < list_length; i++) {
+	if(debug) {
+	    fprintf(log_file, "[%s:%3i] %s(): song to add = %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)song_list->data);
+	    fflush(log_file);
 	}
+	mpd_playlist_queue_add(mpd_info.obj, song_list->data);
+	song_list = g_list_next(song_list);
+    }
 
-	/* add songs */
-
-	for(i = 0; i < list_length; i++) {
-		if(debug) {
-			fprintf(log_file, "[%s:%3i] %s(): song to add = %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)song_list->data);
-			fflush(log_file);
-		}
-		mpd_playlist_queue_add(mpd_info.obj, song_list->data);
-		song_list = g_list_next(song_list);
-	}
-
-	mpd_playlist_queue_commit(mpd_info.obj);
+    mpd_playlist_queue_commit(mpd_info.obj);
 
 
-	/* free song list *
-	g_list_foreach(song_list, (GFunc)g_free, NULL);
-	g_list_free(song_list); */
+    /* free song list *
+    g_list_foreach(song_list, (GFunc)g_free, NULL);
+    g_list_free(song_list); */
 
-	return;
+    return;
 }
 
 /* add directory (gchar*)data to playlist */
 void mpd_add_directory_to_playlist(GtkWidget *widget, gpointer data) {
-	mpd_playlist_add(mpd_info.obj, (char*)data);
-	return;
+    mpd_playlist_add(mpd_info.obj, (char*)data);
+    return;
 }
 
 /*********** local functions **********/
@@ -274,42 +274,42 @@ void mpd_add_directory_to_playlist(GtkWidget *widget, gpointer data) {
 /* callback if a row of the playlist is double-clicked */
 static void pl_row_activated(GtkTreeView *tv, GtkTreePath *path, GtkTreeViewColumn *column, gpointer data) {
 
-	GtkTreeIter iter;
-	GtkTreeModel *model;
-	gint pl_id;
+    GtkTreeIter iter;
+    GtkTreeModel *model;
+    gint pl_id;
 
-	pl_id = -32;
+    pl_id = -32;
 
-	model = gtk_tree_view_get_model(tv);
-	gtk_tree_model_get_iter(model, &iter, path);
-	gtk_tree_model_get(model, &iter, COLUMN_PL_ID, &pl_id, -1);
+    model = gtk_tree_view_get_model(tv);
+    gtk_tree_model_get_iter(model, &iter, path);
+    gtk_tree_model_get(model, &iter, COLUMN_PL_ID, &pl_id, -1);
 
-	if(pl_id > -32) {
-	    // if auto connect is set, try to reconnect
-	    if(mpd_info.msi.connected) {
-	        if(!mpd_check_connected(mpd_info.obj)) {
-		    if(mpd_connect(mpd_info.obj) != MPD_OK) {
-			msi_clear(&mpd_info);
-			mpd_info.msi.connected = FALSE;
-			return;
-		    }
-		    msi_fill(&mpd_info);
-		    mpd_info.msi.connected = TRUE;
+    if(pl_id > -32) {
+	// if auto connect is set, try to reconnect
+	if(mpd_info.msi.connected) {
+	    if(!mpd_check_connected(mpd_info.obj)) {
+		if(mpd_connect(mpd_info.obj) != MPD_OK) {
+		    msi_clear(&mpd_info);
+		    mpd_info.msi.connected = FALSE;
+		    return;
 		}
-            mpd_player_play_id(mpd_info.obj, pl_id);
+		msi_fill(&mpd_info);
+		mpd_info.msi.connected = TRUE;
 	    }
+	    mpd_player_play_id(mpd_info.obj, pl_id);
 	}
-	fprintf(log_file, "[%s:%3i] %s(): pl_id = %i\n", __FILE__, __LINE__, __FUNCTION__, pl_id);
-	fflush(log_file);
+    }
+    fprintf(log_file, "[%s:%3i] %s(): pl_id = %i\n", __FILE__, __LINE__, __FUNCTION__, pl_id);
+    fflush(log_file);
 
-	return;
+    return;
 }
 
 /* handle key press on playlist */
 static void pl_key_pressed(GtkWidget *widget, GdkEventKey *key, gpointer data) {
-	if(key->keyval == GDK_Delete)
-		pl_del_songs(NULL, data);
-	return;
+    if(key->keyval == GDK_Delete)
+	pl_del_songs(NULL, data);
+    return;
 }
 
 /* vim:sts=4:shiftwidth=4
