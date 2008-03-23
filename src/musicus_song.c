9b16f2d8bb05a4e6724464f6f27d5f0cf60594b8
Simon Gerber <simugerber@student.ethz.ch>
2008-03-23 20:44:23 +0100

    Added initialization and destruction methods to musicus_song.c.

--- src/musicus_song.c
+++ src/musicus_song.c
@@ -6,6 +6,21 @@
 
 struct _MusicusSongPrivate {
     GString *song_name;
+    gboolean dispose_has_run;
+};
+
+static GObjectClass *parent_class = NULL;
+
+/* internal methods */
+static void musicus_song_class_init (MusicusSongClass *klass, gpointer class_data);
+static void musicus_song_init (GTypeInstance *instance, gpointer class);
+static void musicus_song_dispose (GObject *object);
+static void musicus_song_finalize (GObject *object);
+
+const gchar *musicus_song_get_song_name(MusicusSong *song) {
+    if(song->priv->dispose_has_run)
+	return NULL;
+    return song->priv->song_name->str;
 }
 
 GType musicus_song_get_type(void) {
@@ -27,6 +42,56 @@ GType musicus_song_get_type(void) {
     }
 
     return type;
+}
+
+static void musicus_song_class_init (MusicusSongClass *klass, gpointer class_data) {
+
+    GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+
+    gobject_class->dispose = musicus_song_dispose;
+    gobject_class->finalize = musicus_song_finalize;
+
+    parent_class = g_type_class_peek_parent (klass);
+
+    g_type_class_add_private (klass, sizeof(MusicusSongPrivate));
+    return;
+}
+
+static void musicus_song_init (GTypeInstance *instance, gpointer class) {
+    MusicusSong *self = MUSICUS_SONG (instance);
+    self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, MUSICUS_SONG_TYPE, MusicusSongPrivate);
+    self->priv->song_name = g_string_new("");
+    self->priv->dispose_has_run = FALSE;
+    return;
+}
+
+static void musicus_song_dispose (GObject *object) {
+
+    MusicusSong *self = MUSICUS_SONG (object);
+
+    /* if dispose did run already, return */
+    if(self->priv->dispose_has_run) {
+	return;
+    }
+
+    /* Make sure dispose does not run twice */
+    self->priv->dispose_has_run = TRUE;
+
+    /* free song_name */
+    g_string_free(self->priv->song_name, TRUE);
+
+    G_OBJECT_CLASS (parent_class)->dispose (object);
+
+    return;
+}
+
+static void musicus_song_finalize (GObject *object) {
+
+    MusicusSong *self = MUSICUS_SONG(object);
+
+    G_OBJECT_CLASS(parent_class)->finalize (object);
+
+    return;
 }
 
 /* vim:sts=4:shiftwidth=4
