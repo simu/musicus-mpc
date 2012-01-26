138c93ba286494543b3308d451c3377ae4317d72
Simon Gerber <simugerber@student.ethz.ch>
2012-01-26 14:20:11 +0100

    Fixed formatting; fixed non-regex splits

--- gnome-shell-extension/extension.js
+++ gnome-shell-extension/extension.js
@@ -1,3 +1,4 @@
+// vim:ts=4 sts=4 sw=4 foldmethod=marker
 const St = imports.gi.St;
 const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
@@ -11,32 +12,32 @@ const Mainloop = imports.mainloop;
 const Panel = imports.ui.panel;
 
 /** {{{ sprintf() for JavaScript 0.7-beta1
-http://www.diveintojavascript.com/projects/javascript-sprintf
+  http://www.diveintojavascript.com/projects/javascript-sprintf
 
-Copyright (c) Alexandru Marasteanu <alexaholic [at) gmail (dot] com>
-All rights reserved.
+  Copyright (c) Alexandru Marasteanu <alexaholic [at) gmail (dot] com> All
+  rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
     * Neither the name of sprintf() for JavaScript nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+	  names of its contributors may be used to endorse or promote products derived
+	  from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Alexandru Marasteanu BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Alexandru Marasteanu BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
@@ -167,22 +168,23 @@ var vsprintf = function(fmt, argv) {
 /** endof sprintf for javascript }}} **/
 
 function Musicus() {
-    this._init();
+	this._init();
 }
 
 Musicus.prototype = {
-    __proto__: PanelMenu.Button.prototype,
+	__proto__: PanelMenu.Button.prototype,
 
-    _init: function(){
-        PanelMenu.Button.prototype._init.call(this, 0.0);
+	_init: function()
+	{
+		PanelMenu.Button.prototype._init.call(this, 0.0);
 
-        this.lang = {
-            'play' : 'Play',
-            'pause' : 'Pause',
-            'stop' : 'Stop',
+		this.lang = {
+			'play' : 'Play',
+			'pause' : 'Pause',
+			'stop' : 'Stop',
 			'prev' : 'Previous',
 			'next' : 'Next'
-        };
+		};
 		this.items = {
 			'Title %s'        : ['_songTitle'],
 			'Song %d of %d'   : ['_songNo','_playlistSongCount'],
@@ -191,55 +193,82 @@ Musicus.prototype = {
 			'Repeat: %s   Random: %s   Single: %s   Consume: %s' : ['_repeat', '_random', '_single', '_consume']
 		};
 
-        this.statusLabel = new St.Label({
-            text: "--",
-            style_class: "sm-label"
-        });
+		this.statusLabel = new St.Label({
+			text: "--",
+			style_class: "sm-label"
+		});
 
-        // destroy all previously created children, and add our statusLabel
-        this.actor.get_children().forEach(function(c) {
-            c.destroy()
-        });
-        this.actor.add_actor(this.statusLabel);
+		// destroy all previously created children, and add our statusLabel
+		this.actor.get_children().forEach(function(c) {
+			c.destroy()
+		});
+		this.actor.add_actor(this.statusLabel);
 		Main.panel._centerBox.add(this.actor, {y_fill:true });
 
-        this._control = this._detectMpc();
+		this._control = this._detectMpc();
 
-        if(this._control===null){
-            this.title='Warning';
-            this.content='Please install mpc. If it doesn\'t help, click here to report!';
-        }
+		if(this._control===null){
+			this.title='Warning';
+			this.content='Please install mpc. If it doesn\'t help, click here to report!';
+		}
 
-        this._update_status();
-        //update every 5 seconds, todo: prefs
-        event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, function () {
-            this._update_status();
-            return true;
-        }));
-    },
+		this._update_status();
+		//update every 5 seconds, todo: prefs
+		event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, function () {
+			this._update_status();
+			return true;
+		}));
+	},
 
-    _detectMpc: function(){
-        //detect if sensors is installed
-        let ret = GLib.spawn_command_line_sync("which mpc");
-        if ( (ret[0]) && (ret[3] == 0) ) {//if yes
-            return ret[1].toString().split("\n", 1)[0];//find the path of the sensors
-        }
-        return null;
-    },
+	_detectMpc: function()
+	{
+		//detect if sensors is installed
+		let ret = GLib.spawn_command_line_sync("which mpc");
+		if ( (ret[0]) && (ret[3] == 0) ) {//if yes
+			return ret[1].toString().split("\n", 1)[0];//find the path of the sensors
+		}
+		return null;
+	},
 
-    _update_status: function() {
-        if (this._control){
+	_update_status: function()
+	{
+		if (this._control){
 			let raw_mpc_output = GLib.spawn_command_line_sync(this._control)[1].toString();
 			this._parse_mpc_output(raw_mpc_output);
 			this._updateMenu(null);
-        }
+		}
 		else {
 			this._updateMenu("Install mpc to use this extension!");
 		}
 	},
 
-	_parse_mpc_output: function(raw_output) {
+	_parse_settings_line: function(line)
+	{
+		let setting_line = line.split(/\s{2,}/);
+		debug(setting_line);
+		this._volume = parseInt(setting_line[0].split(/: ?/)[1].split(/%/)[0]);
+		this._repeat  = setting_line[1].match(/off/)==null;
+		this._random  = setting_line[2].match(/off/)==null;
+		this._single  = setting_line[3].match(/off/)==null;
+		this._consume = setting_line[4].match(/off/)==null;
+	},
+
+	_parse_state_line: function(line)
+	{
+		let state_line = line.split(/\s+/);
+		this._playState = state_line[0];
+		let nos = state_line[1].split('/');
+		this._songNo = parseInt(nos[0].split('#')[1]);
+		this._playlistSongCount = parseInt(nos[1]);
+		let times = state_line[2].split('/');
+		this._currTime = times[0];
+		this._totalTime= times[1];
+	},
+
+	_parse_mpc_output: function(raw_output)
+	{
 		let lines = raw_output.split('\n');
+		// XXX: hack
 		if (lines.length == 2) {
 			this._playState = "[stopped]";
 			this._songTitle="n/a";
@@ -248,41 +277,22 @@ Musicus.prototype = {
 			this._playlistSongCount = -1;
 			this._currTime = -1;
 			this._totalTime= -1;
-			let setting_line = lines[0].split(/   /);
-			this._volume = parseInt(setting_line[0].split(/: /)[1].split(/%/)[0]);
-			this._repeat  = setting_line[1].match(/off/)==null;
-			this._random  = setting_line[2].match(/off/)==null;
-			this._single  = setting_line[3].match(/off/)==null;
-			this._consume = setting_line[4].match(/off/)==null;
+			this._parse_settings_line(lines[0]);
 		}
 		else {
 			this._songTitle = lines[0];
-
-			// does not always work properly.
-			let state_line = lines[1].split(' ');
-			this._playState = state_line[0];
-			let nos = state_line[1].split('/');
-			this._songNo = parseInt(nos[0].split('#')[1]);
-			this._playlistSongCount = parseInt(nos[1]);
-			let times = state_line[2].split('/');
-			this._currTime = times[0];
-			this._totalTime= times[1];
-
-			let setting_line = lines[2].split(/   /);
-			this._volume = parseInt(setting_line[0].split(/: /)[1].split(/%/)[0]);
-			this._repeat  = setting_line[1].match(/off/)==null;
-			this._random  = setting_line[2].match(/off/)==null;
-			this._single  = setting_line[3].match(/off/)==null;
-			this._consume = setting_line[4].match(/off/)==null;
+			this._parse_state_line(lines[1]);
+			this._parse_settings_line(lines[2]);
 		}
 	},
 
-	_updateMenu: function(msg) {
-        this.statusLabel.set_text("MPD: " + this._playState);
+	_updateMenu: function(msg)
+	{
+		this.statusLabel.set_text("MPD: " + this._playState);
 		// clear box
-        this.menu.box.get_children().forEach(function(c) {
-            c.destroy()
-        });
+		this.menu.box.get_children().forEach(function(c) {
+			c.destroy()
+		});
 		if (msg != null) {
 			this.menu.addMenuItem(new PopupMenu.PopupMenuItem(msg));
 		}
@@ -304,19 +314,19 @@ Musicus.prototype = {
 		}
 		Main.panel._centerBox.add(this.actor, {y_fill:true});
 		Main.panel._menus.addMenu(this.menu);
-    }
+	}
 }
 
 //for debugging
 function debug(a){
-    global.log(a);
-    //Util.spawn(['echo',a]);
+	global.log(a);
+	//Util.spawn(['echo',a]);
 }
 
 function start(extMeta) {
-    /*let userExtensionLocalePath = extMeta.path + '/locale';
-    Gettext.bindtextdomain("places_button", userExtensionLocalePath);
-    Gettext.textdomain("places_button");*/
+	/*let userExtensionLocalePath = extMeta.path + '/locale';
+	  Gettext.bindtextdomain("places_button", userExtensionLocalePath);
+	  Gettext.textdomain("places_button");*/
 	new Musicus();
 }
 
@@ -333,11 +343,11 @@ let musicus;
 let event=null;
 
 function enable() {
-    musicus = new Musicus();
+	musicus = new Musicus();
 }
 
 function disable() {
-    musicus.destroy();
+	musicus.destroy();
 	Mainloop.source_remove(event);
 	indicator = null;
 }
